41d057e6b9f792c7987ef53d084d1f3c6966fd38
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-13 18:48:54 +0200

    add header with styles for burger menu and logo

---
+++ src/parts/shared/header.php
@@ -0,0 +1,25 @@
+<header id="header">
+
+  <div class="header-content-wrapper">
+
+    <div class="row header-main">
+      <div class="col4 col3-tablet header-menu-wrapper">
+        <?php get_template_part( 'parts/elements/element', 'burger-menu'); ?>
+        <div class="header-burger-menu-text">MENU</div>
+      </div>
+
+      <div class="col2 col3-tablet header-logo-wrapper">
+        <?php get_template_part( 'parts/shared/logo', 'dark'); ?>
+      </div>
+
+      <div class="col1 col3-tablet header-search-wrapper">
+      </div>
+    </div>
+
+    <div class="row header-quick-links">
+
+    </div>
+
+  </div>
+
+</header>
