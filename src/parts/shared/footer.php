bd297eb17e0651cf61e14298d7a672969666fce5
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-13 14:59:55 +0200

    add footer layout and styling

---
+++ src/parts/shared/footer.php
@@ -0,0 +1,23 @@
+<footer id="footer">
+
+  <div class="row footer-content-wrapper" >
+
+    <div class="col1 col4-desktop footer-col-logo" >
+      <?php get_sidebar( 'footer-col-1' ); ?>
+    </div>
+
+    <div class="col1 col3-tablet col4-desktop footer-col" >
+      <?php get_sidebar( 'footer-col-2' ); ?>
+    </div>
+
+    <div class="col1 col3-tablet col4-desktop footer-col" >
+      <?php get_sidebar( 'footer-col-3' ); ?>
+    </div>
+
+    <div class="col1 col3-tablet col4-desktop footer-col" >
+      <?php get_sidebar( 'footer-col-4' ); ?>
+    </div>
+
+  </div>
+
+</footer>
