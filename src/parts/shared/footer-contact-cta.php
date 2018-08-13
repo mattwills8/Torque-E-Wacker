725c05b8cd4a0362a6a97006e35b025381cd9bde
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-13 15:59:10 +0200

    finish footer contact cta styles

--- src/parts/shared/footer-contact-cta.php
+++ src/parts/shared/footer-contact-cta.php
@@ -2,14 +2,22 @@
 if ( get_field('show_footer_contact_cta') ) {
 ?>
 
-<div class="footer-contact-cta" style="background-image: url('wp-content/uploads/2018/08/IMG_3410.jpg');">
+<div class="row footer-contact-cta" style="background-image: url('wp-content/uploads/2018/08/IMG_3410.jpg');">
 
-  <h2>How To Reach Us</h2>
-  <p>For more information, here are the best ways to get in touch.</p>
+  <div class="footer-contact-cta-content-wrapper">
 
-  <div class="footer-contact-cta-buttons">
-    <button class="bg-white">Contact Us</button>
-    <button class="bg-white button-icon">Leasing</button>
+    <h2>How To Reach Us</h2>
+    <p>For more information, here are the best ways to get in touch.</p>
+
+    <div class="footer-contact-cta-buttons">
+      <div class="button-wrapper">
+        <button class="bg-white">Contact Us</button>
+      </div>
+      <div class="button-wrapper">
+        <button class="bg-white button-icon">Leasing</button>
+      </div>
+    </div>
+
   </div>
 
 </div>
