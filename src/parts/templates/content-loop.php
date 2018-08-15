82dfae5027d7224e4c7ec5636120c59bc0b3a821
mattwills8 <matt_wills8@hotmail.co.uk>
2018-08-15 13:07:22 +0200

    add loop layout and styling

--- src/parts/templates/content-loop.php
+++ src/parts/templates/content-loop.php
@@ -9,10 +9,45 @@
         'cat' => $post_cat
       ) );
 
+      $found_posts = (int) $query->found_posts;
+
+      $post_count = 0;
+
+      // we have to loop for both tablet and desktop
+      // and then hide the one we dont want in the css depending on screen size
       if ( $query->have_posts() ) { while ( $query->have_posts() ) { $query->the_post();
 
-        get_template_part( 'parts/templates/loop', $post->post_type ); 
+        if ($post_count % 2 === 0) { ?>
+          <div class="row wacker-loop-row wacker-loop-row-tablet" >
+        <?php }
 
+            get_template_part( 'parts/templates/loop', $post->post_type );
+
+
+        if ($post_count % 2 === 1 || $post_count === $found_posts - 1) { ?>
+          </div>
+        <?php }
+
+        $post_count++;
+      } }
+
+      $post_count = 0;
+
+      // desktop loop
+      if ( $query->have_posts() ) { while ( $query->have_posts() ) { $query->the_post();
+
+        if ($post_count % 3 === 0) { ?>
+          <div class="row wacker-loop-row wacker-loop-row-desktop" >
+        <?php }
+
+            get_template_part( 'parts/templates/loop', $post->post_type );
+
+
+        if ($post_count % 3 == -1 || $post_count === $found_posts - 1) { ?>
+          </div>
+        <?php }
+
+        $post_count++;
       } }
     ?>
 
--- src/parts/templates/loop-post.php
+++ src/parts/templates/loop-post.php
@@ -1,1 +1,11 @@
+<div class="col1 col2-tablet col3-desktop wacker-loop-post" >
 
+  <h3><?php the_title(); ?></h3>
+
+  <div class="wacker-loop-post-excerpt"><?php the_excerpt(); ?></div>
+
+  <a href="<?php echo the_permalink(); ?>">
+    <div class="body-link">READ MORE</div>
+  </a>
+
+</div>
